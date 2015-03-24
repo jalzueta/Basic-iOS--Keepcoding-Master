dbc7369ac88c39fe169cc004341e9f961eaa9f5b
Javi Alzueta <jalzuetap@gmail.com>
2015-03-24 21:42:32 +0100

    Initial Commit

---
+++ StarWars/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StarWars
+//
+//  Created by Javi Alzueta on 24/3/15.
+//  Copyright (c) 2015 FillinGAPPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
